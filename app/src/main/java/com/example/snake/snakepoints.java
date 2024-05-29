f49afd0e1697b56b949c8ffa2c65ba88b8458c97
Your Name <you@example.com>
2024-05-29 12:53:18 +0300

    Initial commit

---
+++ app/src/main/java/com/example/snake/snakepoints.java
@@ -0,0 +1,38 @@
+package com.example.snake;
+
+public class snakepoints {
+
+    private int positionX, positionY;
+
+    public snakepoints(int positionX, int positionY) {
+        this.positionX = positionX;
+        this.positionY = positionY;
+        }
+
+    public static void clear() {
+    }
+
+    public int getPositionX() {
+        return positionX;
+    }
+
+    public void setPositionX(int positionX) {
+        this.positionX = positionX;
+    }
+
+    public int getPositionY() {
+        return positionY;
+    }
+
+    public void setPositionY(int positionY) {
+        this.positionY = positionY;
+    }
+
+    public void setPositionX() {
+    }
+
+    public void setPositionY() {
+    }
+}
+
+
